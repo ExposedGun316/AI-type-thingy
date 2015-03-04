97717ed41dfebe0b05566eb2da69fd8200bbf55f
ExposedGun316 <exposedgun316@live.co.uk>
2015-03-04 21:41:28 +0000

    Made space at last message

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -29,10 +29,10 @@ int _tmain(int argc, _TCHAR* argv[])
 	cout << "Oh, you like " + hobbies + ", thats so cool! I have one hobby, it's sitting staring into the abyss." << "\n" "Whats your favourite subject in school?";
 	
 	cin >> subject;
-	cout << "Hmmm... your favourite is " + subject + "?Thats cool, I never went to school." << "\n" "Well, I'm going to head off. Cya later :)";
+	cout << "Hmmm... your favourite is " + subject + "?Thats cool, I never went to school." << "\n" "Well, I'm going to head off. Cya later :) ";
 	
 	cin >> farewell;
-	cout << "Cya next time maybe! Bai!";
+	cout << "Cya next time maybe! Bai! ";
 
 	
 }
