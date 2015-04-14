390e4a14914aa4c681391cd48f2d8a04b2bbcbcc
ExposedGun316 <exposedgun316@live.co.uk>
2015-04-14 21:13:41 +0100

    Changed some wording in the AI's answers

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -23,10 +23,10 @@ int _tmain(int argc, _TCHAR* argv[])
 	cout << "You're " + age + "? Thats cool, I'm 500 years old." << "\n" "How are you today?";
 
 	cin >> mood;
-	cout << "Oh, so your in a " + mood + " mood? I see. I'm good in case you were wondering how I am."  << "\n" "What are your hobbies?";
+	cout << "Your in a " + mood + " mood? I see. I'm good in case you were wondering how I am."  << "\n" "What are your hobbies?";
 
 	cin >> hobbies;
-	cout << "Oh, you like " + hobbies + ", thats so cool! I have two hobby, it's sitting staring into the abyss and waiting for you." << "\n" "Whats your favourite subject in school?";
+	cout << "You like " + hobbies + ", thats so cool! I have two hobby, it's sitting staring into the abyss and waiting for you." << "\n" "Whats your favourite subject in school?";
 	
 	cin >> subject;
 	cout << "Hmmm... your favourite is " + subject + "?Thats cool, I never went to school because I am so smart... yea..." << "\n" "Well, I'm going to shut off for a bit, hopefully we'll meet again. Cya later :) ";
